f1ff709f503821e882df3b43cf957a0ea57853dd
duy <duy123galaxy13@gmail.com>
2019-08-09 14:24:46 +0700

    Initial Commit

---
+++ messageApp_/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  messageApp_
+//
+//  Created by Lê Duy on 8/9/19.
+//  Copyright © 2019 Lê Duy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
